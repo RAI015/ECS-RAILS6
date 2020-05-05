b3b0bc6085d8eebb5e374707e93abbbf16de3759
RAI015 <i.scryed72342019@gmail.com>
2020-05-05 16:23:36 +0900

    CircleCI Deploy test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
     @posts = Post.all
 
     # render json: @posts
-    render html: 'watame ganbare!! \n watame night fever!!'
+    render html: 'watame ganbare!! \n watame night fever!! \n 2020/05/05 ver.'
   end
 
   # GET /posts/1
